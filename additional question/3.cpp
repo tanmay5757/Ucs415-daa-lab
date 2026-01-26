d1bb678ce66d2a2b907360f8357673f47dd6dfc3
tanmay5757 <tanmaysingh1404@gmail.com>
2026-01-26 22:52:34 +0530

    Create 3.cpp

---
+++ additional question/3.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {4, 2, 6, 9, 2};
+    
+    cout << "Original array: ";
+    printArray(arr);
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
